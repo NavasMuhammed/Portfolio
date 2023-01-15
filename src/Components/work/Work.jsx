0cf3f9d42d18d70210650c9d76d1b0f085a91aeb
NavasMuhammed <navas3444a3@gmail.com>
2023-01-15 16:52:45 +0530

    feat: resume-update

--- src/Components/work/Work.jsx
+++ src/Components/work/Work.jsx
@@ -1,59 +1,86 @@
 import "./work.scss";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useRef, useState } from "react";
 
 export default function Work() {
- 
-  return (
+  const { ref, inView } = useInView({
+    threshold:1
+  });
 
-    <div  className="work" id="work">
-      <div className="left">
-        <div className="technical" >
-          <br/>
-            <div className="new">Technical Skills</div>
-            <div className="test">HTML</div>
-            
-            <div className="html">
-              <div className="inner"></div>
-            </div><div className="test">CSS</div>
-            <div className="css">
-            <div className="inner"></div>
-            </div><div className="test">JS</div>
-            <div className="javascript">
-            <div className="inner"></div>
-            </div><div className="test">Python</div>
-            <div className="python">
-            <div className="inner"></div>
-            </div><div className="test">C</div>
-            <div className="c">
-            <div className="inner"></div>
-            </div>
+  useEffect(() => {
+    
+    if (inView) {
+      
+      const html = document.getElementById("html");
+      html.classList.add("active");
+      const css = document.getElementById("css");
+      css.classList.add("active");
+      const javascript = document.getElementById("javascript");
+      javascript.classList.add("active");
+      const python = document.getElementById("python");
+      python.classList.add("active");
+      const c = document.getElementById("c");
+      c.classList.add("active");
+      const react = document.getElementById("react");
+      react.classList.add("active");
+      const tech = document.getElementById("technical");
+      tech.classList.add("active");
+
+
+    } else {
+      const html = document.getElementById("html");
+      html.classList.remove("active");
+      const css = document.getElementById("css");
+      css.classList.remove("active");
+      const javascript = document.getElementById("javascript");
+      javascript.classList.remove("active");
+      const python = document.getElementById("python");
+      python.classList.remove("active");
+      const c = document.getElementById("c");
+      c.classList.remove("active");
+      const react = document.getElementById("react");
+      react.classList.remove("active");
+      const tech = document.getElementById("technical");
+      tech.classList.remove("active");
+    }
+  }, [inView]);
+
+  return (
+    <div className="work" id="work">
+      <div className="left" ref={ref}>
+        <div  className="technical" id="technical">
+          <br />
+          <div className="new">Technical Skills</div>
+          <div className="test">React</div>
+          <div className="progressbar">
+            <div className="inner react" id="react"></div>
+          </div>
+          <div className="test">HTML</div>
+
+          <div className="progressbar">
+            <div className="inner html " id="html"></div>
+          </div>
+          <div className="test">CSS</div>
+          <div className="progressbar">
+            <div className="inner css " id="css"></div>
+          </div>
+          <div className="test">JS</div>
+          <div className="progressbar">
+            <div className="inner javascript " id="javascript"></div>
+          </div>
+          <div className="test">Python</div>
+          <div className="progressbar">
+            <div className="inner python " id="python"></div>
+          </div>
+          <div className="test">C</div>
+          <div className="progressbar">
+            <div className="inner c" id="c"></div>
+          </div>
           
         </div>
       </div>
       <div className="right">
-      <div className="technical" >
-          <br/>
-            <div className="new">Languages Skills</div>
-            <div className="test">ENGLISH</div>
-            
-            <div className="html">
-              <div className="inner"></div>
-            </div><div className="test">MALAYALAM</div>
-            <div className="css">
-            <div className="inner"></div>
-            </div><div className="test">HINDI</div>
-            <div className="javascript">
-            <div className="inner"></div>
-            </div><div className="test">ARABIC</div>
-            <div className="python">
-            <div className="inner"></div>
-            </div>
-            {/* <div className="test">C</div> */}
-            {/* <div className="c">
-            <div className="inner"></div>
-            </div>
-           */}
-        </div>
-      
+       
       </div>
     </div>
   );
